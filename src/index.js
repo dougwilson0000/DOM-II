e8f63766d56a27efabe749e61232ab1b2dd7f407
Douglas Wilson <dougwilson0000@gmail.com>
2021-11-30 21:24:50 -0500

    finished 1-5 of addeventlistener!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// #1
+const logoText = document.querySelector('.logo-heading');
+logoText.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'blue';
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 600);
+    }, false);
+
+// #2
+const topImage = document.querySelector('.intro img');
+topImage.addEventListener('dblclick', function (event) {
+    event.target.style.transform = 'scale(.5)';
+} )
+
+// #3
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+    })
+})
+
+// #4
+const introP = document.querySelector('.intro p');
+introP.addEventListener('contextmenu', event => {
+    event.preventDefault();
+})
+
+// #5
+document.addEventListener('copy', event => {
+    const copyText = document.getSelection();
+    event.clipboardData.setData('text/plain', copyText.toString().toUpperCase());
+    event.preventDefault();
+})
+
+// #6
+
+
+// #7
+
+
+// #8
+
+
+// #9
+
+
+// #10
